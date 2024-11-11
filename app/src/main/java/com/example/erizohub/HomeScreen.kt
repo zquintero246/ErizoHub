3014524be6ae49ca12e93c441eec28b9ab424088
Andrejulian21 <julian21arteaga@gmail.com>
2024-11-10 23:08:00 -0500

    home screen
    oiknib

--- app/src/main/java/com/example/erizohub/HomeScreen.kt
+++ app/src/main/java/com/example/erizohub/HomeScreen.kt
@@ -6,11 +6,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -19,54 +24,72 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
 
 
 @Composable
 fun HomeScreen(navController: NavController){
-    val emprendimiento1 = Emprendimiento(
-        "Emprendimiento x",
-        "Mi emprendimiento se tratatatatattatatatatata",
-        "https://www.pexels.com/es-es/foto/mar-blanco-y-negro-hombre-playa-27741970/",
-        "https://www.pexels.com/es-es/foto/mar-blanco-y-negro-hombre-playa-27741970/",
-    )
-    val listEmprendimientos: List<Emprendimiento> = listOf(
-        emprendimiento1
-    )
+    val listEmprendimientos = remember {
+        mutableStateOf<List<Emprendimiento>>(emptyList())
+    }
+    LaunchedEffect(Unit) {
+        val db = Firebase.firestore
+        val response = db.collection("emprendimientos").get().await()
+        val emprendimientos = response.documents.map{ document ->
+            Emprendimiento(
+                nombre_emprendimiento = document.getString("nombre_emprendimiento")?: "",
+                descripcion= document.getString("descripcion")?: "",
+                imagenEmprendimiento = document.getString("imagenEmprendimiento")?: "",
+                imagenes = document.get("imagenes") as List<String>? ?: emptyList()
 
+            )
+        }
+        listEmprendimientos.value= emprendimientos
+    }
 
     LazyColumn (
-        verticalArrangement = Arrangement.spacedBy(14.dp),
-        modifier = Modifier.fillMaxSize()
+        verticalArrangement = Arrangement.spacedBy(14.dp)
     ){
-        items(listEmprendimientos.size){
-            Emprendimientoitem(miEmprendimiento = listEmprendimientos.get(it))
+        items(listEmprendimientos.value.size){
+            Emprendimientoitem(myEmprendimiento = listEmprendimientos.value[it])
         }
     }
 }
 
 
+
+
 @Composable
-fun Emprendimientoitem(miEmprendimiento: Emprendimiento){
-    Card {
-        Row {
+fun Emprendimientoitem(myEmprendimiento: Emprendimiento) {
+    Card(modifier = Modifier.padding(8.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             AsyncImage(
-                model = miEmprendimiento.banner,
-                contentDescription = "Banner image",
+                model = myEmprendimiento.imagenEmprendimiento,
+                contentDescription = null,
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(16f / 9f),
-                contentScale = ContentScale.Crop,
+                    .size(100.dp)
+                    .padding(end = 12.dp),
+                contentScale = ContentScale.Crop
             )
-            Column(modifier = Modifier.padding(12.dp)) {
-                Text(text = miEmprendimiento.nombre_emprendimiento,
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = myEmprendimiento.nombre_emprendimiento,
                     fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(bottom = 6.dp)
-
+                    modifier = Modifier.padding(bottom = 4.dp)
                 )
-                Text(text = miEmprendimiento.description)
-
-
+                Text(
+                    text = "Email: ${myEmprendimiento.descripcion}",
+                    fontSize = 16.sp
+                )
             }
         }
     }
